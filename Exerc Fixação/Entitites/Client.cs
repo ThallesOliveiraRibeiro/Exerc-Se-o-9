af4be947bf43b322d6fa622d80cfa4b8f9429d9e
Thalles Oliveira <manobalboa@outlook.com>
2020-06-24 20:41:22 -0300

    First commit

---
+++ Exerc Fixação/Entitites/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_Fixação.Entitites
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+        public override string ToString()            //Client: Alex Green (15/03/1985) - alex@gmail.com
+        {
+
+            return Name
+                + ", ("
+                + BirthDate.ToString("dd/MM/yyyy")
+                + ") - "
+                + Email;
+        }
+    }
+}
